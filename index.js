c31db85f60cfba67a943bd301f55ac0ed0b46bf6
thomaslein <thomaslein@gmail.com>
2014-02-08 21:17:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-var polyfill = require('util.polyfill')
-	, easing = require('util.easing')
-	, identify = require('util.identify')
-	, css = require('dom.css')
+require('requestAnimationFrame-polyfill');
+// Date.now() polyfill
+Date.now = Date.now
+	|| function () { return new Date().getTime() }
+
+var style = require('style')
+	, bind = require('object-utils').bind
+	, isFunction = require('lodash.isfunction')
+	, isString = require('lodash.isstring')
 	, win = window
-	, doc = win.document
-	, FRAME_RATE = 60
-	, DEFAULT_DURATION = 0.5
+	, doc = window.document
+
 	, anims = {}
-	, length = 0
+	, size = 0
 	, pool = []
 	, uid = 1
 	, tick = 0
 	, last = 0
 	, running = false
-	, csstransitions = css.csstransitions;
 
-// TODO: move to utils.function
-var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+	, FRAME_RATE = 60
+	, DEFAULT_DURATION = 500
+	, DEFAULT_EASING = require('ease/lib/cubic').outCubic
+	, POOL_SIZE = 10;
 
-// API
 module.exports = animate;
+
+// Populate object pool
+for (var i = 0, n = POOL_SIZE; i < n; i++) {
+	pool.push(new Anim());
+}
 
 /**
  * Retrieve an Anim instance bound to 'target'
  * Set 'keep' to true to prevent cleanup
  * @param {Object} target
- * @param {Boolean} keep
+ * @param {Boolean} [keep]
  * @returns {Anim}
  */
-function animate(target, keep) {
+function animate (target, keep) {
+	if (!target) return;
+
 	// reuse from the object pool
 	var anim = pool.length ? pool.pop() : new Anim();
 	anim.id = uid++;
@@ -41,7 +52,7 @@ function animate(target, keep) {
  * Add 'anim' to render loop
  * @param {Anim} anim
  */
-function add(anim) {
+function add (anim) {
 	if (!anims[anim.id]) {
 		anims[anim.id] = anim;
 		anim.isRunning = true;
@@ -50,8 +61,8 @@ function add(anim) {
 		if (!running) {
 			running = true;
 			tick = 0;
-			last = +(new Date);
-			_onTick();
+			last = Date.now();
+			onTick();
 		}
 	}
 }
@@ -60,35 +71,38 @@ function add(anim) {
  * Remove 'anim' from render loop
  * @param {Anim} anim
  */
-function remove(anim) {
-	anim.isRunning = false;
-	delete anims[anim.id];
-	length--;
-	// Stop if none active
-	if (!length) running = false;
+function remove (anim) {
+	if (anim.isRunning) {
+		anim.isRunning = false;
+		anim.isComplete = false;
+		anim.duration = 0;
+		anim.elapsed = 0;
+		anim.delayBefore = 0;
+		anim.delayAfter = 0;
+		anim.tickCallbacks = [];
+		anim.completeCallbacks = [];
+		anim.properties = {};
+		anim.ease = DEFAULT_EASING;
+		anim.usingCssTransitions = false;
+		delete anims[anim.id];
+		length--;
+		// Stop if none active
+		if (!length) running = false;
+	}
 }
 
 /**
  * Destroy 'anim'
  * @param {Anim} anim
  */
-function destroy(anim) {
+function destroy (anim) {
 	// Check that this is eligible for destruction
 	if (anim.id) {
-		if (anim.isRunning) remove(anim);
+		remove(anim);
 		// Reset
 		anim.id = null;
 		anim.target = null;
-		anim.duration = 0;
-		anim.elapsed = 0;
-		anim.properties = {};
-		anim.easing = easing['outCubic'];
-		anim.tickCallbacks = [];
-		anim.completeCallbacks = [];
 		anim.keep = false;
-		anim.isRunning = false;
-		anim.useCssTransitions = false;
-		anim.transitionStyle = '';
 		pool.push(anim);
 	}
 }
@@ -97,51 +111,159 @@ function destroy(anim) {
  * Tick handler
  * @param {Number} time
  */
-function _onTick(time) {
-	var now = +(new Date)
+function onTick (time) {
+	var now = Date.now()
 		, tick = now - last;
 	// Store
 	last = now;
 	for (var id in anims) {
-		anims[id]._render(tick);
+		render(anims[id], tick);
 	}
 	// Loop
-	if (running) win.requestAnimationFrame(_onTick);
+	if (running) win.requestAnimationFrame(onTick);
 }
+
+
+/**
+ * Render
+ * @param {Anim} anim
+ * @param {Number} time
+ */
+function render (anim, time) {
+	var props = anim.properties
+		, b, c, callback, callbacks, dur, propObj, value;
+
+	anim.elapsed += time;
+	// Make sure total time does not exceed duration
+	dur = (anim.elapsed < anim.duration)
+		? anim.elapsed
+		: anim.duration;
+
+	// Handle delay before
+	if (anim.delayBefore > 0) {
+		anim.delayBefore -= time;
+		// Round down if under 1 frame
+		if (anim.delayBefore < 16) {
+			anim.delayBefore = 0;
+		}
+	}
+
+	// Update properties
+	if (!anim.isComplete && anim.delayBefore <= 0) {
+		for (var prop in props) {
+			propObj = props[prop];
+			// All types except css transitions
+			if (propObj.type < 4) {
+				b = propObj.start;
+				c = propObj.end - b;
+				value = propObj.current = anim.ease.js(dur, b, c, anim.duration);
+				switch (propObj.type) {
+					case 1:
+						anim.target[prop](value);
+						break;
+					case 2:
+						anim.target[prop] = value;
+						break;
+					case 3:
+						style.setStyle(anim.target, prop, "" + value + propObj.unit);
+				}
+			}
+		}
+	}
+
+	// Call tick callbacks
+	executeCallbacks(anim.tickCallbacks);
+
+	// On complete...
+	if (anim.elapsed >= anim.duration) {
+		anim.isComplete = true;
+
+		// Handle delay after
+		if (anim.delayAfter) {
+			anim.duration += anim.delayAfter;
+			anim.delayAfter = 0;
+		} else {
+			// Remove css transition syntax
+			if (anim.usingCssTransitions) {
+				style.clearStyle(anim.target, 'transition');
+				anim.usingCssTransitions = false;
+			}
+
+			// Reset
+			callbacks = anim.completeCallbacks.slice();
+			(anim.keep) ? remove(anim) : destroy(anim);
+
+			// Trigger callbacks
+			// Delay to allow for GC and cleanup
+			setTimeout(function() {
+				executeCallbacks(callbacks);
+				callbacks = null;
+			}, 0);
+		}
+	}
+};
+
+/**
+ * Execute one or more 'callbacks'
+ * @param {Array}
+ */
+function executeCallbacks (callbacks) {
+	if (callbacks.length) {
+		// Don't loop if only 1
+		if (callbacks.length == 1) {
+			callback = callbacks[0];
+			callback.args
+				?	callback.func.apply(null, callback.args)
+				: callback.func();
+		} else {
+			for (var i = 0, n = callbacks.length; i < n; i++) {
+				callback = callbacks[i];
+				callback.args
+					? callback.func.apply(null, callback.args)
+					: callback.func();
+			}
+		}
+	}
+};
 
 /**
  * Anim class
  */
-function Anim() {
+function Anim () {
 	this._render = bind(this._render, this);
 	this.id = null;
 	this.target = null;
 	this.duration = 0;
+	this.delayBefore = 0;
+	this.delayAfter = 0;
 	this.elapsed = 0;
 	this.properties = {};
-	this.easing = easing['outCubic'];
+	this.ease = DEFAULT_EASING;
 	this.tickCallbacks = [];
 	this.completeCallbacks = [];
-	this.isRunning = false;
 	this.keep = false;
-	this.useCssTransitions = false;
-	this.transitionStyle = '';
+	this.isRunning = false;
+	this.isComplete = false;
+	this.usingCssTransitions = false;
 }
 
 /**
  * Animate from existing values to target values
  * @param {Object} properties
- * @param {Number} duration (miliseconds)
- * @param {String} ease
+ * @param {Number} [duration] (miliseconds)
+ * @param {Object} [ease]
+ * @returns {Anim}
  */
-Anim.prototype.to = function(properties, duration, ease) {
-	var current, end, p, s1, val;
+Anim.prototype.to = function (properties, duration, ease) {
+	var current, end, p, val, tStyle;
+
+	if (ease) this.ease = ease;
 	if (duration == null) duration = DEFAULT_DURATION;
-	if (ease != null) this.easing = easing[ease];
-	this.duration = duration * 1000;
+	this.duration += duration;
 	this.elapsed = 0;
 	this.properties = {};
-	this.useCssTransitions = false;
+	this.usingCssTransitions = false;
+
 	for (var prop in properties) {
 		val = properties[prop];
 		p = {
@@ -150,51 +272,74 @@ Anim.prototype.to = function(properties, duration, ease) {
 			end: val,
 			type: 0
 		};
+
 		// Property is a Function
-		if (identify.isFunction(this.target[prop])) {
+		if (isFunction(this.target[prop])) {
 			p.start = this.target[prop]();
 			p.type = 1;
+
 		// Property is property
 		} else if (prop in this.target) {
 			p.start = this.target[prop];
 			p.type = 2;
+
 		// Property is css
 		} else {
-			current = css.getNumericStyle(this.target, prop);
+			current = style.getNumericStyle(this.target, prop);
 			p.start = current[0];
+
 			// Use ending unit if a string is passed
-			if (identify.isString(val)) {
-				end = css.parseNumber(val, prop);
+			if (isString(val)) {
+				end = style.parseNumber(val, prop);
 				p.unit = end[1];
 				p.end = end[0];
+
 			// Use the current unit if none specified
 			} else {
 				p.unit = current[1];
 				p.end = val;
 			}
+
 			// Use css transitions if available
-			if (csstransitions) {
+			if (style.hasTransitions) {
 				// First set up transition
-				if (!this.useCssTransitions) {
-					s1 = (this.target.getAttribute('style') || '').length;
-					css.setStyle(this.target, {
-						'transition-property': 'all',
-						'transition-duration': "" + this.duration + "ms"
-					});
-					if (ease) css.setStyle(this.target, 'transition-timing-function', easing.css[ease]);
-					// Protect against styles getting translated to shorthand (Chrome)
-					this.transitionStyle = this.target.getAttribute('style').slice(s1);
-					this.useCssTransitions = true;
+				if (!this.usingCssTransitions) {
+					tStyle = 'all ' + this.duration + 'ms';
+					if (ease) tStyle += ' ' + (this.ease.css || DEFAULT_EASING.css);
+					if (this.delayBefore) tStyle += ' ' + this.delayBefore + 'ms';
+					style.setStyle(this.target, {transition: tStyle});
+					this.usingCssTransitions = true;
 				}
 				p.type = 4;
-				css.setStyle(this.target, prop, p.end + p.unit);
+				style.setStyle(this.target, prop, p.end + p.unit);
 			} else {
 				p.type = 3;
 			}
 		}
 		this.properties[prop] = p;
 	}
+
 	add(this);
+
+	// Chain
+	return this;
+};
+
+/**
+ * Delay the start or completion of an animation
+ * @param {Number} duration
+ */
+Anim.prototype.delay = function (duration) {
+	if (duration != null) {
+		if (!this.isRunning) {
+			this.duration += duration;
+			this.delayBefore = duration;
+			add(this);
+		} else {
+			this.delayAfter = duration;
+		}
+	}
+
 	// Chain
 	return this;
 };
@@ -204,7 +349,7 @@ Anim.prototype.to = function(properties, duration, ease) {
  * @param {String} property
  * @returns {Object}
  */
-Anim.prototype.getProperty = function(property) {
+Anim.prototype.getProperty = function (property) {
 	if (this.isRunning) {
 		var prop = this.properties[property];
 		return prop != null ? prop.current : null;
@@ -216,39 +361,49 @@ Anim.prototype.getProperty = function(property) {
  * @param {String} property
  * @param {Object} value
  */
-Anim.prototype.setProperty = function(property, value) {
+Anim.prototype.setProperty = function (property, value) {
 	if (this.isRunning) {
 		var prop = this.properties[property];
 		if (prop != null) prop.end = value;
+		// Set new end target for css transitions
+		if (prop.type == 4) style.setStyle(this.target, property, value);
 	}
 };
 
 /**
- * Register tick callback handler
+ * Register tick callback handler with optional arguments
  * @param {Function} callback
  * @param {...}
+ * @returns {Anim}
  */
-Anim.prototype.onTick = function(callback) {
-	var args = 2 <= arguments.length ? Array.slice.call(arguments, 1) : [];
+Anim.prototype.onTick = function (callback) {
+	var args = (2 <= arguments.length)
+		? Array.prototype.slice.call(arguments, 1)
+		: null;
 	this.tickCallbacks.push({
 		func: callback,
 		args: args
 	});
+
 	// Chain
 	return this;
 };
 
 /**
- * Register complete callback handler
+ * Register complete callback handler with optional arguments
  * @param {Function} callback
  * @param {...}
+ * @returns {Anim}
  */
-Anim.prototype.onComplete = function(callback) {
-	var args = 2 <= arguments.length ? Array.slice.call(arguments, 1) : [];
+Anim.prototype.onComplete = function (callback) {
+	var args = (2 <= arguments.length)
+		? Array.prototype.slice.call(arguments, 1)
+		: null;
 	this.completeCallbacks.push({
 		func: callback,
 		args: args
 	});
+
 	// Chain
 	return this;
 };
@@ -256,94 +411,19 @@ Anim.prototype.onComplete = function(callback) {
 /**
  * Stop running Anim
  */
-Anim.prototype.stop = function() {
-	if (this.isRunning) {
-		if (this.keep) {
-			remove(this);
-		} else {
-			destroy(this);
-		}
+Anim.prototype.stop = function () {
+	if (this.keep) {
+		remove(this)
+		return this;
+	} else {
+		return destroy(this);
 	}
 };
 
 /**
  * Destroy Anim
  */
-Anim.prototype.destroy = function() {
+Anim.prototype.destroy = function () {
 	destroy(this);
 	return null;
-};
-
-/**
- * Render
- * @param {Number} time
- */
-Anim.prototype._render = function(time) {
-	var props = this.properties
-		, b, c, callback, cbs, dur, propObj, value;
-	this.elapsed += time;
-	// Make sure total time does not exceed duration
-	dur = this.elapsed < this.duration ? this.elapsed : this.duration;
-	for (var prop in props) {
-		propObj = props[prop];
-		if (propObj.type < 4) {
-			b = propObj.start;
-			c = propObj.end - b;
-			value = propObj.current = this.easing(dur, b, c, this.duration);
-			switch (propObj.type) {
-				case 1:
-					this.target[prop](value);
-					break;
-				case 2:
-					this.target[prop] = value;
-					break;
-				case 3:
-					css.setStyle(this.target, prop, "" + value + propObj.unit);
-			}
-		}
-	}
-
-	// Call tick callbacks
-	if (this.tickCallbacks.length) {
-		// Don't loop if only 1
-		if (this.tickCallbacks.length == 1) {
-			this.tickCallbacks[0].func.apply(null, this.tickCallbacks[0].args);
-		} else {
-			cbs = this.tickCallbacks;
-			for (var i = 0, n = cbs.length; i < n; i++) {
-				callback = cbs[i];
-				callback.func.apply(null, callback.args);
-			}
-		}
-	}
-
-	// On complete...
-	if (this.elapsed >= this.duration) {
-		// Remove css transition syntax
-		if (this.useCssTransitions) {
-			this.target.setAttribute('style', this.target.getAttribute('style').replace(this.transitionStyle, ''));
-			this.useCssTransitions = false;
-			this.transitionStyle = '';
-		}
-		// Reset
-		cbs = this.completeCallbacks.slice();
-		this.tickCallbacks = [];
-		this.completeCallbacks = [];
-		this.properties = {};
-		(this.keep) ? remove(this) : destroy(this);
-		// Trigger callbacks
-		if (cbs.length) {
-			// Delay 1 frame to allow for GC and cleanup
-			win.requestAnimationFrame(function() {
-				if (cbs.length === 1) {
-					cbs[0].func.apply(null, cbs[0].args);
-				} else {
-					for (var i = 0, n = cbs.length; i < n; i++) {
-						callback.func.apply(null, cbs[i].args);
-					}
-				}
-				cbs = null;
-			});
-		}
-	}
 };
